afbdfc7b12ba5d3d115189db6ae676922e0c0354
Harvin Molyneux <hrmolyneux@gmail.com>
2022-09-20 22:36:20 +0100

    fixing intents issue

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if sys.platform == "linux":
         sys.exit(0)
         
 #bot intents to allow for reaction roles
-intents = discord.Intents().all()
+intents = discord.Intents(messages=True, guilds=True)
 bot = commands.Bot(command_prefix='!', intents=intents)
     
 if "mush-bot" not in os.getcwd().lower():
