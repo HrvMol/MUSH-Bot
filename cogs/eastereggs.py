0d8a650082aa37cde886ce5f3858524e45a2041e
Steven Deibert <stevekdeibert@gmail.com>
2022-10-26 17:47:02 -0700

    Changed the format for inserting images

--- cogs/eastereggs.py
+++ cogs/eastereggs.py
@@ -22,38 +22,45 @@ class EasterEggs(commands.Cog):
 
     @commands.command(name="best")
     async def best(self, ctx):
-        await ctx.channel.send("back Boris \nhttps://tinyurl.com/5yrzz5x6")
+        image = "https://tinyurl.com/5yrzz5x6"
+        await ctx.channel.send("Back Boris" + image)
         await ctx.message.delete()
 
     @commands.command(name="nephew")
     async def nephew(self, ctx):
-        await ctx.channel.send("head of dinosaurs \nhttps://tinyurl.com/bdfmkupv")
+        image = "https://tinyurl.com/bdfmkupv"
+        await ctx.channel.send("Head of the dinosaurs" + image)
         await ctx.message.delete()
 
     @commands.command(name="potatoes")
     async def potatoes(self, ctx):
-        await ctx.channel.send("POV: you voted Tories \nhttps://tenor.com/view/boom-bomb-car-bomb-explosion-detonate-gif-15743196")
+        image = "https://tenor.com/view/boom-bomb-car-bomb-explosion-detonate-gif-15743196"
+        await ctx.channel.send("POV: you voted Tories" + image)
         await ctx.message.delete()
 
     @commands.command(name="salty")
     async def salty(self, ctx):
-        await ctx.channel.send("2 world wars, 1 world cup and one womens Euros \nhttps://tinyurl.com/2m3reh38")
+        image = "https://tinyurl.com/2m3reh38"
+        await ctx.channel.send("2 world wars, 1 world cup and 1 women's Euros" + image)
         await ctx.message.delete()
 
     #update 1
     @commands.command(name="russianbias")
     async def russianbias(self, ctx):
-        await ctx.channel.send("https://tinyurl.com/4e2x43nd")
+        image = "https://tinyurl.com/4e2x43nd"
+        await ctx.channel.send(image)
         await ctx.message.delete()
 
     @commands.command(name="scottsman")
     async def scotsman(self, ctx):
-        await ctx.channel.send("https://cdn.discordapp.com/attachments/987513534227316789/1003431507597205584/received_272561204831416.jpeg")
+        await ctx.channel.send(
+        "https://cdn.discordapp.com/attachments/987513534227316789/1003431507597205584/received_272561204831416.jpeg")
         await ctx.message.delete()
 
     @commands.command(name="posh")
     async def posh(self, ctx):
-        await ctx.channel.send("https://cdn.discordapp.com/attachments/987513534227316789/1005955570991370380/unknown.png")
+        await ctx.channel.send(
+            "https://cdn.discordapp.com/attachments/987513534227316789/1005955570991370380/unknown.png")
         await ctx.message.delete()
     
 def setup(client):
