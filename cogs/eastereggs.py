b6c6eab42f0fe60009e6487509626c019bafde8f
Steven Deibert <stevekdeibert@gmail.com>
2022-11-04 20:03:20 -0700

    Update eastereggs.py

--- cogs/eastereggs.py
+++ cogs/eastereggs.py
@@ -3,15 +3,17 @@ from discord.ext import commands
 import random
 
 
-class EasterEggs(commands.Cog):
-	def __init__(self, bot):
+class EasterEggs(commands.Cog):  # Creates a class for the cog that inherits from commands.Cog
+	# this class is used to create a cog, which is a module that can be added to the bot
+
+	def __init__(self, bot):  # this is a special method that is called when the cog is loaded
 		self.bot = bot
 
-	@commands.Cog.listener()
-	async def on_ready(self):
+	@commands.Cog.listener()  # Adds an event listener to the cog
+	async def on_ready(self):  # Called when the bot is ready
 		print("Easter Egg Cog Loaded")
 
-	@commands.Cog.listener()
+	@commands.Cog.listener()  # Adds an event listener to the cog
 	async def on_message(self, ctx):
 		print()
 		if ctx.author != self.bot.user:
@@ -20,48 +22,48 @@ class EasterEggs(commands.Cog):
 			if random.randint(0, 2500) == 1:
 				await ctx.channel.send("fuck you")
 
-	@commands.command(name = "best")
+	@commands.command(name = "best")  # Creates a prefixed command
 	async def best(self, ctx):
 		image = "https://tinyurl.com/5yrzz5x6"
 		await ctx.channel.send("Back Boris" + image)
 		await ctx.message.delete()
 
-	@commands.command(name = "nephew")
+	@commands.command(name = "nephew")  # Creates a prefixed command
 	async def nephew(self, ctx):
 		image = "https://tinyurl.com/bdfmkupv"
 		await ctx.channel.send("Head of the dinosaurs" + image)
 		await ctx.message.delete()
 
-	@commands.command(name = "potatoes")
+	@commands.command(name = "potatoes")  # Creates a prefixed command
 	async def potatoes(self, ctx):
 		image = "https://tenor.com/view/boom-bomb-car-bomb-explosion-detonate-gif-15743196"
 		await ctx.channel.send("POV: you voted Tories" + image)
 		await ctx.message.delete()
 
-	@commands.command(name = "salty")
+	@commands.command(name = "salty")  # Creates a prefixed command
 	async def salty(self, ctx):
 		image = "https://tinyurl.com/2m3reh38"
 		await ctx.channel.send("2 world wars, 1 world cup and 1 women's Euros" + image)
 		await ctx.message.delete()
 
-	@commands.command(name = "russianbias")
+	@commands.command(name = "russianbias")  # Creates a prefixed command
 	async def russianbias(self, ctx):
 		image = "https://tinyurl.com/4e2x43nd"
 		await ctx.channel.send(image)
 		await ctx.message.delete()
 
-	@commands.command(name = "scottsman")
+	@commands.command(name = "scottsman")  # Creates a prefixed command
 	async def scotsman(self, ctx):
 		await ctx.channel.send(
 			"https://cdn.discordapp.com/attachments/987513534227316789/1003431507597205584/received_272561204831416.jpeg")
 		await ctx.message.delete()
 
-	@commands.command(name = "posh")
+	@commands.command(name = "posh")  # Creates a prefixed command
 	async def posh(self, ctx):
 		await ctx.channel.send(
 			"https://cdn.discordapp.com/attachments/987513534227316789/1005955570991370380/unknown.png")
 		await ctx.message.delete()
 
 
-def setup(client):
-	client.add_cog(EasterEggs(client))
+def setup(bot):  # This is called by Pycord to set up the cog
+	bot.add_cog(EasterEggs(bot))  # Adds the cog to the bot
