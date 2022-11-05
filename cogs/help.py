42d0b78da1b7fa2c390fefca71ac3ec889ccd2ec
Steven Deibert <stevekdeibert@gmail.com>
2022-11-05 11:11:12 -0700

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,13 +1,17 @@
+import aiofiles
 import discord
 
 
 class Help(discord.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
+        self.bot.help = ''
 
     @discord.Cog.listener()
     async def on_ready(self):
         print("Help Cog Loaded")
+        async with aiofiles.open("help.md", mode = "r") as file:
+            self.bot.explanation = await file.read()
 
     @discord.slash_command(description="Help command for the bot")
     async def help(self, ctx):
