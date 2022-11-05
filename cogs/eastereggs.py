47e70ae67809d0b18e21499ba5fa9731241fbb4a
Steven Deibert <stevekdeibert@gmail.com>
2022-11-05 10:47:53 -0700

    Update eastereggs.py

--- cogs/eastereggs.py
+++ cogs/eastereggs.py
@@ -24,44 +24,41 @@ class EasterEggs(discord.Cog):  # Creates a class for the cog that inherits from
 	@discord.slash_command(name = "best")  # Creates a prefixed command
 	async def best(self, ctx):
 		image = "https://tinyurl.com/5yrzz5x6"
-		await ctx.channel.send("Back Boris \n" + image)
-		await ctx.message.delete()
+		await ctx.channel.send("Back Boris")
+		await ctx.channel.send(image)
 
 	@discord.slash_command(name = "nephew")  # Creates a prefixed command
 	async def nephew(self, ctx):
 		image = "https://tinyurl.com/bdfmkupv"
-		await ctx.channel.send("Head of the dinosaurs" + image)
-		await ctx.message.delete()
+		await ctx.channel.send("Head of the dinosaurs")
+		await ctx.channel.send(image)
 
 	@discord.slash_command(name = "potatoes")  # Creates a prefixed command
 	async def potatoes(self, ctx):
 		image = "https://tenor.com/view/boom-bomb-car-bomb-explosion-detonate-gif-15743196"
-		await ctx.channel.send("POV: you voted Tories" + image)
-		await ctx.message.delete()
+		await ctx.channel.send("POV: you voted Tories")
+		await ctx.channel.send(image)
 
 	@discord.slash_command(name = "salty")  # Creates a prefixed command
 	async def salty(self, ctx):
 		image = "https://tinyurl.com/2m3reh38"
-		await ctx.channel.send("2 world wars, 1 world cup and 1 women's Euros" + image)
-		await ctx.message.delete()
+		await ctx.channel.send("2 world wars, 1 world cup and 1 women's Euros")
+		await ctx.channel.send(image)
 
 	@discord.slash_command(name = "russianbias")  # Creates a prefixed command
 	async def russianbias(self, ctx):
 		image = "https://tinyurl.com/4e2x43nd"
 		await ctx.channel.send(image)
-		await ctx.message.delete()
 
 	@discord.slash_command(name = "scottsman")  # Creates a prefixed command
 	async def scotsman(self, ctx):
-		await ctx.channel.send(
-			"https://cdn.discordapp.com/attachments/987513534227316789/1003431507597205584/received_272561204831416.jpeg")
-		await ctx.message.delete()
+		image = "https://cdn.discordapp.com/attachments/987513534227316789/1003431507597205584/received_272561204831416.jpeg"
+		await ctx.channel.send(image)
 
 	@discord.slash_command(name = "posh")  # Creates a prefixed command
 	async def posh(self, ctx):
-		await ctx.channel.send(
-			"https://cdn.discordapp.com/attachments/987513534227316789/1005955570991370380/unknown.png")
-		await ctx.message.delete()
+		image = "https://cdn.discordapp.com/attachments/987513534227316789/1005955570991370380/unknown.png"
+		await ctx.channel.send(image)
 
 
 def setup(bot):  # This is called by Pycord to set up the cog
