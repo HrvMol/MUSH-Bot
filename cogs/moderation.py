fda4e83e05a4a77383ef4920a2bf7cb690c7523c
Steven Deibert <stevekdeibert@gmail.com>
2022-11-04 23:30:46 -0700

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,18 +1,17 @@
 import discord
 from discord import Option
-from discord.ext import commands
 
 
-class Moderation(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+class Moderation(discord.Cog):
+    def __init__(self, bot: discord.Bot):
         self.bot = bot
 
-    @commands.Cog.listener()
+    @discord.Cog.listener()
     async def on_ready(self):
         print("Moderation Cog Loaded")
 
     @discord.slash_command(description="remove a certain number of messages")
-    @commands.has_permissions(manage_messages = True)
+    # @discord.has_permissions(manage_messages = True)
     async def purge(self, ctx, messages: Option(int, description="How may messages do you want to purge?",
                                                 requires=True)):
         i = await ctx.channel.purge(limit = messages)
